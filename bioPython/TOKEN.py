79632f99ceeb7f424e589fb57d0fdc479136e7ab
divan <hlinakcurbag@gmail.com>
2023-12-24 17:15:11 +0300

    написала конспектик https://xn--80abvyzg.xn--p1ai/%D0%B1%D0%B8%D0%BE%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%82%D0%B8%D0%BA%D0%B0/ и старые задания/файлы

---
+++ bioPython/TOKEN.py
@@ -0,0 +1,1 @@
+EMAIL = "hlinakcurbag@gmail.com"
---
+++ teach_in_bioinf/train.py
@@ -0,0 +1,8 @@
+def find_max(nums):
+    max_num = float("-inf") # smaller than all other numbers
+    for num in nums:
+        if num > max_num:
+            max_num = num
+    return max_num
+
+print(find_max([10, 456, 98, 78, -45]))
